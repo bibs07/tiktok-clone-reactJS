2cc5f3a13acb10c62aa73b0dcc410605787e7ebf
bibs07 <52453292+bibs07@users.noreply.github.com>
2021-08-24 00:30:51 +1000

    init: Initializing the app. Refactor files.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxd3hD8eyfRmvkJ18_oxveYqcSmCcftfY",
+  authDomain: "tiktok-clone-a4934.firebaseapp.com",
+  projectId: "tiktok-clone-a4934",
+  storageBucket: "tiktok-clone-a4934.appspot.com",
+  messagingSenderId: "380124508394",
+  appId: "1:380124508394:web:1adf0df21c51c9032ba793",
+  measurementId: "G-01GRJVGYG9",
+};
